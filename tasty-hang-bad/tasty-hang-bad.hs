b200154f95d6c0ec20aac5de228e678fdb106ecb
John Ky <john.ky@iohk.io>
2025-07-13 23:13:39 +1000

    Initial project

---
+++ tasty-hang-bad/tasty-hang-bad.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+module Main where
+
+import Prelude
+
+import Control.Monad (void)
+import Data.ByteString qualified as BS
+import Data.List qualified as L
+import System.Environment qualified as E
+
+import Hedgehog qualified as H
+import Test.Tasty qualified as T
+import Test.Tasty.HUnit
+import Test.Tasty.Ingredients qualified as T
+
+{- HLINT ignore "Use let" -}
+
+tests :: IO T.TestTree
+tests = do
+  t1 <-
+    pure $
+      testCaseInfo "t1" $ do
+        void . H.check . H.withTests 1 . H.withShrinks 0 . H.property $ do
+          void . H.evalIO $ BS.readFile "README.md"
+        pure "done"
+
+  pure $
+    T.testGroup
+      "test/cardano-cli-golden/cardano-cli-golden.hs"
+      (L.replicate 170 t1)
+
+ingredients :: [T.Ingredient]
+ingredients = T.defaultIngredients
+
+main :: IO ()
+main = do
+  args <- E.getArgs
+  E.withArgs ([] <> args) $ tests >>= T.defaultMainWithIngredients ingredients
